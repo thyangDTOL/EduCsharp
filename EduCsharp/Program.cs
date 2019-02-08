3f19d63c417c4b7a86cc0caf1b10568d63d460d7
thyang <thyang@dtol.co.kr>
2019-02-08 18:18:11 +0900

    Update License

--- EduCsharp/Program.cs
+++ EduCsharp/Program.cs
@@ -10,7 +10,7 @@ namespace EduCsharp
     {
         static void Main(string[] args)
         {
-            throw new System.Exception("TestDay1_Q01 프로젝트부터 실행하세요");
+            throw new System.Exception("TestDay1_Q01 프로젝트부터 시작입니다.");
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # EduCsharp
-C# 내부 교육자료
+DTOL C# 내부 교육자료
+
+## 1. 2018-01-25
+TestDay1
